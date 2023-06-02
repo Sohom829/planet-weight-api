a1e4ad8ca89aa08fe5d8c4ee6fa5412383519980
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-06-02 16:40:06 +0600

    Add "/" router

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express from "express";
+import express, { Request, Response } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import router from "./routes/v1/converter.routes";
@@ -9,5 +9,14 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(helmet());
 app.use("/api/v1/converter", router);
+app.get("/", (req: Request, res: Response) => {
+  const s = req.query.s;
+  res
+    .json({
+      message: "Success",
+    })
+    .status(200);
+  return s;
+});
 
 export default app;
